40585167a2cc82e2267df2bee75bde37cfc7f13f
pedro diaz <pjdaze@gmail.com>
2018-06-07 02:13:18 -0400

    stable

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
+import Fab6 from '../images/fab6.jpg';
+import Fab1 from '../images/fab1.jpg';
+import Fab7 from '../images/fac7.jpg';
 
 import IntroWrapper from './containers/IntroWrapper';
 
 const Intro = () => {
   return (
     <IntroWrapper className="flex-center">
-      <h2>Welcome To My Awesomeness</h2>
+      <div className="flex-center intro-top">
+        <p className="one intro-animatedRight">Executive Producer</p>
+        <p className="two intro-animatedLeft">Creartor</p>
+        <p className="three intro-animatedRight">Lider</p>
+        <p className="four intro-animatedLeft">Futurist</p>
+      </div>
+      <section className="intro-bio">
+        <p>
+          My name is Fabian Q, i'm awesome because i know Mother Monster I like to spend my free
+          time visualizing new ideas that i can apply to my daily work like and im loving every
+          moment so far i love Art, design music and moshrroms, i hate other cats and im in love
+          with showbiz lets see maybe one day.
+        </p>
+
+        <div className="intro-images">
+          <img src={Fab6} alt="fabian with patrick starrr" />
+          <img src={Fab1} alt="fabian with patrick starrr" />
+          <img src={Fab7} alt="fabian with patrick starrr" />
+        </div>
+      </section>
     </IntroWrapper>
   );
 };
--- src/components/containers/IntroWrapper.js
+++ src/components/containers/IntroWrapper.js
@@ -3,7 +3,46 @@ import styled from 'styled-components';
 
 const IntroWrapper = styled.div`
   border-bottom: 1px solid #ccc;
-  height: 50vh;
+
+  justify-content: center;
+  text-align: center;
+  .intro-top {
+    p {
+      font-size: 1.5em;
+      line-height: 1.8em;
+      text-transform: uppercase;
+      animation-duration: 0.3s;
+    }
+  }
+  .intro-bio {
+    width: 95%;
+    max-width: 600px;
+    padding: 50px;
+    font-family: 'Open Sans', sans-serif;
+    font-size: 300;
+    line-height: 1.5em;
+  }
+  .intro-images {
+    justify-self: center;
+
+    height: 300px;
+    margin: 0 auto;
+    width: 95%;
+    max-width: 700px;
+    img {
+      width: 300px;
+    }
+  }
+
+  .two {
+    animation-delay: 0.8s;
+  }
+  .three {
+    animation-delay: 1.1s;
+  }
+  .four {
+    animation-delay: 1.3s;
+  }
 `;
 
 export default IntroWrapper;
